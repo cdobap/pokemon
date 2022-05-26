f9467884454bb76ce7302ba32715adc6f35470e9
zahr91 <karima.moha.ali@gmail.com>
2022-05-26 11:58:48 +0200

    modification

---
+++ karima.py
@@ -0,0 +1,18 @@
+gros exo
+class pokemon():
+    
+    name=""
+    hp=""
+    
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, var_name):
+        self.name = var_name
+    
+    def attack(self):
+        print(self.name + " attaque")
+
+
+    pikachu=pokemon()
+        
